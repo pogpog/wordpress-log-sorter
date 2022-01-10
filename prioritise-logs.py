ab50e79fc7dd49be094e2186769a424a10071044
Oliver Fokerd <oliver.fokerd@hallam.co.uk>
2022-01-10 15:33:43 +0000

    Compile regex for 2x speed up

--- prioritise-logs.py
+++ prioritise-logs.py
@@ -7,7 +7,7 @@ start_time = time.time()
 
 # Replace with your file names
 # read_file = 'debug.log'
-read_file = 'files/debug.log'
+read_file = 'files/debug.old-01.log'
 
 # Auto-increment output file name
 output_version = 0
@@ -23,21 +23,27 @@ total = 0
 
 num_results = 100
 
+regex1 = re.compile(r"\[[^\]]+\]\s?(.*)")
+regex2 = re.compile(r"^=")
 # Populate tally of log entries
 with open(read_file) as f1:
     d = {}  # Dictionary for tally
     lines = f1.readlines()
     for line in lines:
         total += 1
+
         # Remove date from start of line
-        line = re.sub(r"\[[^\]]+\]\s?(.*)", r"\1", line)
-        line = re.sub(r"^=", "'=", line)
+        line = regex1.sub(r"\1", line)
+        # line = regex2.sub("'=", line) # Escape the equals sign for google sheets
+
         # Remove trailing \n
         line = line.rstrip("\n")
         if line in d:
             d[line] += 1
         else:
             d[line] = 1
+
+print(f'Done populating: {time.time() - start_time} seconds')
 
 # Populate lists for DataFrame columns
 for key in d:
@@ -51,4 +57,4 @@ df.sort_values(['count'], inplace=True, ascending=False)
 top_rows = df.head(num_results)
 top_rows.to_csv(write_file, index=False)
 
-print(f'finished in {time.time() - start_time} seconds')
+print(f'Finished: {time.time() - start_time} seconds')
